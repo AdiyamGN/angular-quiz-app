5e301ef1d922f16d6dc96befe9669a459f1060fe
Adiyamseged <adiyamgn@outlook.com>
2016-12-19 19:34:19 -0500

    finished doing the routing for the page using Ui-route. for reference look at config.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
 
 	"use strict";
 
-	angular.module('quizApp', []);
+	angular.module('quizApp', ['ui.router']);
 
 })();
---
+++ js/controllers/home-controller.js
@@ -0,0 +1,14 @@
+(function() {
+
+	'use strict';
+
+	angular
+		.module('quizApp')
+		.controller('homeCtrl', homeCtrl)
+
+
+		function homeCtrl() {
+			var vm = this;
+		}
+
+})();
